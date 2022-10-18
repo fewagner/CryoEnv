1878edd9b0a1c9c3a22ef18fed1471a0ddb7f611
Kolos Niedermayer <kolos.niedermayer@gmail.com>
2022-10-18 20:48:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
-setup(name='cryoenv',
-      version='1.0.1',
-      install_requires=['gym',
-                        'stable_baselines3',
-                        'tqdm',
-                        'numba',
-                        ]
-      )
+setup(
+    name="cryoenv",
+    version="1.0.1",
+    packages=["cryoenv"],
+    package_dir={"cryoenv": "./cryoenv"},
+    python_requires=">=3.8,<3.10",
+    install_requires=[
+        "gym==0.19.0",
+        "stable_baselines3==1.3.0",
+        "tqdm==4.62.3",
+        "numba==0.54.1",
+        "torch==1.12.1",
+        "numbalsoda==0.3.4",
+    ],
+)
