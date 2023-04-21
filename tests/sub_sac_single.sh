ece065f457830a01867eed4b433f8468a45963d4
Felix Wagner <felix.wagner@oeaw.ac.at>
2023-04-21 18:26:18 +0200

    try fixing transformer

---
+++ tests/sub_sac_single.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# execution script for SLURM cluster 
+
+# for double tes: put to time=480
+
+name="li1l"
+idx=29
+echo "Submitting job ${name} version ${idx}"
+JOB_NAME="SAC_${name}_${idx}"
+SCRIPT_PATH="/users/felix.wagner/cryoenv/tests/train_sac.py"
+CONTAINER_PATH="/users/felix.wagner/mosquitto_latest.sif"
+OUTPUT_FILE="/users/felix.wagner/outputs/${JOB_NAME}.out"
+SBATCH_OPTIONS=" -c 1 --mem=4G --output=${OUTPUT_FILE} --job-name=${JOB_NAME} --time=240"
+SINGULARITY_OPTIONS=" -c -B /eos/ -H /users/felix.wagner"
+PYTHON_OPTIONS=" -u "
+CMD_ARGUMENTS=" --version ${idx} --detector ${name} --rnd_seed ${idx} --alt_seed 10000 --scale 0.2 --lr 3e-4  --batch_size 16  --gamma 0.9  --gradient_steps 20"  # --double_tes
+
+sbatch ${SBATCH_OPTIONS} --wrap="time singularity exec ${SINGULARITY_OPTIONS} ${CONTAINER_PATH} python3 ${PYTHON_OPTIONS} ${SCRIPT_PATH} ${CMD_ARGUMENTS}"
+
+exit 0
--- tests/sub_sac_training.sh
+++ tests/sub_sac_training.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # execution script for SLURM cluster 
 
-DOUBLE_TES=false
+# for double tes: put to time=600
 
 for name in li1p li1l li2p
 do
-  for (( idx=0; idx<=4; idx++))
+  for (( idx=105; idx<=109; idx++))
   do
     echo "Submitting job ${name} version ${idx}"
     JOB_NAME="SAC_${name}_${idx}"
@@ -15,7 +15,7 @@ do
     SBATCH_OPTIONS=" -c 1 --mem=4G --output=${OUTPUT_FILE} --job-name=${JOB_NAME} --time=240"
     SINGULARITY_OPTIONS=" -c -B /eos/ -H /users/felix.wagner"
     PYTHON_OPTIONS=" -u "
-    CMD_ARGUMENTS=" --version ${idx} --detector ${name} --rnd_seed ${idx} --double_tes ${DOUBLE_TES}"
+    CMD_ARGUMENTS=" --version ${idx} --detector ${name} --scale 0.2 --lr 1e-3  --batch_size 64  --gamma 0.99  --gradient_steps 20  --tau 20.  --sweep " 
 
     sbatch ${SBATCH_OPTIONS} --wrap="time singularity exec ${SINGULARITY_OPTIONS} ${CONTAINER_PATH} python3 ${PYTHON_OPTIONS} ${SCRIPT_PATH} ${CMD_ARGUMENTS}"
   done
